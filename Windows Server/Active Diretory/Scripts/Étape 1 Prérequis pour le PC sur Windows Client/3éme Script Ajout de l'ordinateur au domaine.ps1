1a631d6b2140e150b60b7d3a58007804e0829dd0
Thomas BRAUD <thomas.braud04@gmail.com>
2025-09-23 17:40:41 +0200

    erreur dans la numérotation des scritps

--- Windows Server/Active Diretory/Scripts/Étape 1 Prérequis pour le PC sur Windows Client/3éme Script Ajout de l'ordinateur au domaine.ps1
+++
@@ -1,9 +0,0 @@
-﻿$domain = "remoteworks.local"
-$domainExists = (Get-WmiObject -Class Win32_ComputerSystem).Domain
-
-# Vérifiez si l'ordinateur est déjà membre du domaine
-if ($domainExists -ne $domain) {
-    Add-Computer -DomainName $domain -Credential Administrateur
-} else {
-    Write-Host "L'ordinateur est déjà membre du domaine $domain."
-}
