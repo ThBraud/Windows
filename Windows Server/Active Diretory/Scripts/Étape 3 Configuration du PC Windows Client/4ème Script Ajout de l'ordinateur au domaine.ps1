b277385c7c229770a3678de58f54806e6208149d
ThBraud <thomas.braud04@gmail.com>
2025-09-23 17:56:09 +0200

    Add files via upload

---
+++ Windows Server/Active Diretory/Scripts/Étape 3 Configuration du PC Windows Client/4ème Script Ajout de l'ordinateur au domaine.ps1
@@ -0,0 +1,9 @@
+﻿$domain = "remoteworks.local"
+$domainExists = (Get-WmiObject -Class Win32_ComputerSystem).Domain
+
+# Vérifiez si l'ordinateur est déjà membre du domaine
+if ($domainExists -ne $domain) {
+    Add-Computer -DomainName $domain -Credential Administrateur
+} else {
+    Write-Host "L'ordinateur est déjà membre du domaine $domain."
+}
